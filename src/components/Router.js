6c6a02cde46cf6c95d2097dfb7f401e951af3092
KirbyPatrick <pkirbyinatx@gmail.com>
2019-06-10 22:36:05 -0500

    added Routes for recipe and recipe by id

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,9 +2,15 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import App from "../App";
+import Recipe from "./Recipe";
 
 const Router = () => (
   <BrowserRouter>
-    <Route path="/" component={App} />
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/recipe/:id" component={Recipe} />
+    </Switch>
   </BrowserRouter>
 );
+
+export default Router;
